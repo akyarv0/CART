981050dc9703010392a116a80a71362a1801f04b
akyarv0 <akyarv0@gmail.com>
2024-03-14 16:24:49 +0300

    first commit

---
+++ cart yeni/app.js
@@ -0,0 +1,13 @@
+//SELECTORS
+
+const btn = document.querySelector(".btn");
+const table = document.querySelector(".table");
+const count = document.querySelector(".count");
+const cartSum = document.querySelector(".cartSum");
+const addedItemsTbody = document.querySelector(".addedItems");
+
+//FUNCTIONS
+
+function updateCount() {
+    count.innerHTML = countVal;
+}
